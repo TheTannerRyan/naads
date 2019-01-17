ba13d24a6af8a85766a41267c1a58cba20176d21
Tanner Ryan <canadatechguy@gmail.com>
2019-01-17 15:32:44 -0500

    Update liveness timeout from 65 to 75 seconds

--- naads.go
+++ naads.go
@@ -12,7 +12,7 @@ import (
 	"github.com/thetannerryan/cap"
 )
 
-const version = "v0.0.8" // NAADS client version
+const version = "v0.0.9" // NAADS client version
 
 // Client represents the configuration for the NAAD client.
 type Client struct {
